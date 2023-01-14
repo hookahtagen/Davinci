baa115cad5771f7593bef7ceb0160fba3d4743f6
hookahtagen <hendrik.siemens@my.com>
2023-01-14 18:26:33 +0100

    .

---
+++ random_1.py
@@ -0,0 +1,15 @@
+import math
+
+u_limit = 100000000
+
+def sieve_of_eratosthenes(n):
+    primes = [True] * (n + 1)
+    primes[0] = primes[1] = False
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if primes[i]:
+            for j in range(i * i, n + 1, i):
+                primes[j] = False
+    return [i for i in range(len(primes)) if primes[i]]
+
+
+print(sieve_of_eratosthenes(u_limit))
